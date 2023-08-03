26d41bc80515e3d0e8b53908801eed98f9138f30
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-08-04 01:07:23 +0200

    Using Finally Block

---
+++ JavaGroupA/src/Book2/chapter8/CrazyWithZeros.java
@@ -0,0 +1,27 @@
+package Book2.chapter8;
+
+public class CrazyWithZeros {
+    public static void main(String[] args) {
+        try {
+            int answer = divideTheseNumbers(5, 0);
+        } catch (Exception e) {
+            System.out.println("Tried twice, still didn't work!");
+        }
+    }
+
+    static int divideTheseNumbers(int a, int b) {
+        int c;
+        try {
+            c = a / b;
+            System.out.println("It worked!");
+        } catch (Exception e){
+            System.out.println("Didn't work the first time.");
+            c = a / b;
+            System.out.println("It worked the second time!");
+        }finally{
+            System.out.println("Let's clean the mess");
+        }
+        System.out.println("It worked after all.");
+        return c;
+    }
+}
