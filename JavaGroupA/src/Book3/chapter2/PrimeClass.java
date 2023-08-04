9cddd8211fea7c8b15547c44aa109bb90eb97fd7
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-08-04 13:58:04 +0200

    Using Initializer

---
+++ JavaGroupA/src/Book3/chapter2/PrimeClass.java
@@ -0,0 +1,11 @@
+package Book3.chapter2;
+import java.util.Scanner;
+public class PrimeClass {
+    private Scanner sc = new Scanner(System.in);
+    public int x;
+    {
+        System.out.print(
+                "Enter the starting value for x: ");
+        x = sc.nextInt();
+    }
+}
