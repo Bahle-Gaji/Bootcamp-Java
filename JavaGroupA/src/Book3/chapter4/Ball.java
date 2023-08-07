af75a371483318e10af530c0b9dd7cf61907d039
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-08-07 08:18:29 +0200

    Protecting methods

---
+++ JavaGroupA/src/Book3/chapter4/Ball.java
@@ -0,0 +1,15 @@
+package Book3.chapter4;
+
+public class Ball {
+    private double weight;
+
+    protected double getWeight() {
+        return this.weight;
+    }
+
+    protected void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
+
+
---
+++ JavaGroupA/src/Book3/chapter4/BaseBall.java
@@ -0,0 +1,7 @@
+package Book3.chapter4;
+
+public class BaseBall extends Ball {
+    public BaseBall() {
+        setWeight(5.125);
+    }
+}
