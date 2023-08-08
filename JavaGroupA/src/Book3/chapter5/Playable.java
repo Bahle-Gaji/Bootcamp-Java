11d5c95acc220d58cd7dd96934255af3fe2136d6
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-08-08 10:42:02 +0200

    Using interfaces, implement play method

---
+++ JavaGroupA/src/Book3/chapter5/Playable.java
@@ -0,0 +1,5 @@
+package Book3.chapter5;
+
+public interface Playable {
+    void play();
+}
---
+++ JavaGroupA/src/Book3/chapter5/TicTacToe.java
@@ -0,0 +1,9 @@
+package Book3.chapter5;
+
+public class TicTacToe implements Playable{
+    @Override
+    public void play() {
+        System.out.println("Playing a tictactoe game.");
+    }
+}
+
