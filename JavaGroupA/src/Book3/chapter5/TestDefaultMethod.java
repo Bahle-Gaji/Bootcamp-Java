493c8fd41fc1a4e07ddb563eb9ad06b83d56d5ae
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-08-11 10:26:08 +0200

    Adding default methods to interfaces
    Here we test whether the default method quit() added to the Playable interface works succesfully.

---
+++ JavaGroupA/src/Book3/chapter5/TestDefaultMethod.java
@@ -0,0 +1,9 @@
+package Book3.chapter5;
+
+public class TestDefaultMethod {
+    public static void main(String[] args) {
+        Game g = new Game();
+        g.play();
+        g.quit();
+    }
+}
