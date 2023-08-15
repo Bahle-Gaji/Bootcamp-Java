3802688d23bd66b24c1170efea36ef1c55c181a1
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-08-15 09:17:13 +0200

    Creating own package
    Create util package and import it into TestingPackages

---
+++ JavaGroupA/src/Book3/chapter8/TestingPackages.java
@@ -0,0 +1,16 @@
+package Book3.chapter8;
+
+//import java.util.*;        //import what you're using
+//import java.util.ArrayList;   //import one class at a time
+
+import com.gmail.bahlegaji01.util.Console;
+
+public class TestingPackages {
+    //    ArrayList arrayList = new ArrayList();
+    public static void main(String[] args) {
+        while (Console.askYorN("Keep going?")) {
+            System.out.println("D'oh! ");
+        }
+    }
+}
+
