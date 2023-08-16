bda9d1896b4d7d4b84ba5be3f0dd9a09ea0c01c3
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-08-16 13:59:15 +0200

    Using Enhanced loop

---
+++ JavaGroupA/src/Book4/chapter2/EnhancedLoop.java
@@ -0,0 +1,12 @@
+package Book4.chapter2;
+
+public class EnhancedLoop {
+    static String[] days = {"Sunday", "Monday", "Tuesday",
+            "Wednesday", "Thursday", "Friday", "Saturday"};
+
+    public static void main(String[] args) {
+        for(String day : days){
+            System.out.println(day);
+        }
+    }
+}
