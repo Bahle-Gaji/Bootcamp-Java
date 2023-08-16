9614353e9401a95bf5e64a5400b99f82e308dd6d
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-08-16 14:16:56 +0200

    Using method to print out array

--- JavaGroupA/src/Book4/chapter2/EnhancedLoop.java
+++ JavaGroupA/src/Book4/chapter2/EnhancedLoop.java
@@ -3,15 +3,23 @@ package Book4.chapter2;
 public class EnhancedLoop {
 
     public static void main(String[] args) {
-        for(String day : getDaysOfWeek()){
-            System.out.println(day);
-        }
+//        for(String day : getDaysOfWeek()){
+//            System.out.println(day);
+//        }
+        printStringArray(getDaysOfWeek());
     }
+
 
     public static String[] getDaysOfWeek() {
         String[] days = {"Sunday", "Monday", "Tuesday",
                 "Wednesday", "Thursday", "Friday", "Saturday"};
 
         return days;
+    }
+
+    public static void printStringArray(String[] strings){
+        for(String s : strings){
+            System.out.println(s);
+        }
     }
 }
