fca53edd1ed4329e2ab146f9957b412e14541184
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-08-16 13:54:35 +0200

    Looping arrays with scanner

--- JavaGroupA/src/Book4/chapter2/Arrays.java
+++ JavaGroupA/src/Book4/chapter2/Arrays.java
@@ -6,13 +6,8 @@ public class Arrays {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        //    String names[];
-        //    String[] names = new String[10];  //proper array declaration variations
-        System.out.print("How many players? ");
-
-        int count = sc.nextInt();
-        String[] players = new String[count];
-        System.out.println("There are " + players.length + " players.");
+//            String names[];
+//            String[] names = new String[10];  //proper array declaration variations
 
         /* Initializing arrays */
         String[] days = new String[7];
@@ -25,7 +20,28 @@ public class Arrays {
         days[6] = "Saturday";       //adding elements one at a time
 
         String[] days2 = {"Sunday", "Monday", "Tuesday",
-                "Wednesday", "Thursday", "Friday", "Saturday"};     //adding all the elements at once
+        "Wednesday", "Thursday", "Friday", "Saturday"};     //adding all the elements at once
+
+        int[] numbers = new int[100];
+//        for(int i = 0; i < 100; i++){
+//            numbers[i] = (int) (Math.random() * 100) + 1;
+//            System.out.println(numbers[i]);
+//        }
+
+        System.out.print("How many players? ");
+        int count = sc.nextInt();
+        sc.nextLine();
+
+        String[] players = new String[count];
+        for (int i = 0; i < count; i++)
+        {
+            System.out.print("Enter player name: ");
+            players[i] = sc.nextLine();
+        }
+        System.out.println("Players are: ");
+        for (int i = 0; i < count; i++){
+            System.out.println(players[i]);
+        }
 
     }
 }
