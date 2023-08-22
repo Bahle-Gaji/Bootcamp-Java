eced7ed565494673efa1e4f590f6b6691abc1d0c
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-08-22 23:41:28 +0200

    Complete exercise 1

---
+++ D83527GC10_labs/src/main/java/com/bahlegaji01/chapter04/Exercise4_1.java
@@ -0,0 +1,16 @@
+package com.bahlegaji01.chapter04;
+
+public class Exercise4_1 {
+    
+    public static void main(String[] args) {
+         // Declare and initialize String variables.  Do not initialize message yet.
+        String custName = "John Doe", itemDesc = "1kg of potatoes";
+        String message;
+        
+        // Assign the message variable 
+        message = custName + " wants to purchase " + itemDesc;
+        
+        // Print and run the code
+        System.out.println(message);
+    }
+}
