cef3657b2edeaae953a95b34cb77636ae0526fa8
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-08-22 23:50:11 +0200

    Change values according to example

--- D83527GC10_labs/src/main/java/com/bahlegaji01/chapter04/Exercise4_1.java
+++ D83527GC10_labs/src/main/java/com/bahlegaji01/chapter04/Exercise4_1.java
@@ -4,11 +4,11 @@ public class Exercise4_1 {
     
     public static void main(String[] args) {
          // Declare and initialize String variables.  Do not initialize message yet.
-        String custName = "John Doe", itemDesc = "1kg of potatoes";
+        String custName = "Mary Smith", itemDesc = "shirt";
         String message;
         
         // Assign the message variable 
-        message = custName + " wants to purchase " + itemDesc;
+        message = custName + " wants to purchase a " + itemDesc;
         
         // Print and run the code
         System.out.println(message);
