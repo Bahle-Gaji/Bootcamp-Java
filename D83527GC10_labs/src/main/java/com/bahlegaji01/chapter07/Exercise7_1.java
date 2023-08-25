9197b3870cf6cfa3d72ee1ef675996d1c0511d3f
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-08-25 23:47:55 +0200

    Complete Exercise

---
+++ D83527GC10_labs/src/main/java/com/bahlegaji01/chapter07/Exercise7_1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bahlegaji01.chapter07;
+
+/**
+ *
+ * @author bahle
+ */
+public class Exercise7_1 {
+    public static void main (String[] args){
+        String custName = "Steve Smith";
+        String firstName;
+        int spaceIdx;
+        
+        // Get the index of the space character (" ") in custName. 
+        spaceIdx = custName.indexOf(" ");
+        
+        // Use the substring method parse out the first name and print it.
+        firstName = custName.substring(0, spaceIdx);
+        System.out.println(firstName);
+
+
+    }
+}
