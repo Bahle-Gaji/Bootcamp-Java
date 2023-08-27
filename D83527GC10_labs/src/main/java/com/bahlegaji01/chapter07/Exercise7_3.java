4b41a5ad5e6dea282fbb4a516e299d1e94bd87c4
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-08-27 19:12:16 +0200

    Complete Exercise 7-3

---
+++ D83527GC10_labs/src/main/java/com/bahlegaji01/chapter07/Exercise7_3.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bahlegaji01.chapter07;
+
+/**
+ *
+ * @author bahle
+ */
+public class Exercise7_3 {
+    public static void main(String[] args){
+        int int1;
+        
+	//  Declare and initialize variables of type long, float, and char.
+        long a = 59339283223L;
+        float b = 689;
+        char c = 'c';
+        
+        
+        // Print the long variable.
+        System.out.println("long var: " + a);
+
+	// Assign the long to the int and print the int variable.
+        int1 = (int) a;
+        System.out.println("longToInt: " + int1);
+        
+    }
+    
+}
