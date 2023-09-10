7cd5b362d8ed4307cf9761ebe7a7a7f4e073e4f4
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-09-10 04:15:30 +0200

    Create custom constructor

--- D83527GC10_labs/src/main/java/com/bahlegaji01/chapter09/Customer.java
+++ D83527GC10_labs/src/main/java/com/bahlegaji01/chapter09/Customer.java
@@ -4,7 +4,15 @@ public class Customer {
 
     private String name;
     private String ssn;
-
+    
+    //Ex 9-2
+    //Add a custom constructor
+    public Customer(String name, String ssn){
+        this.name = name;
+        this.ssn = ssn;
+    }
+    
+    //Ex 9-1
     // Encapsulate this class.  Make ssn read only.
     public String getName() {
         return this.name;
