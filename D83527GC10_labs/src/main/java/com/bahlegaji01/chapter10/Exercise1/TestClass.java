786b5f58a4155d759883d52621786bcffd2fe651
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-09-15 13:11:02 +0200

    Complete Exercise 1

--- D83527GC10_labs/src/main/java/com/bahlegaji01/chapter10/Exercise1/TestClass.java
+++ D83527GC10_labs/src/main/java/com/bahlegaji01/chapter10/Exercise1/TestClass.java
@@ -1,18 +1,20 @@
 
-package ex10_1_exercise;
+package com.bahlegaji01.chapter10.Exercise1;
 
 
 public class TestClass {
 
     public static void main(String args[]) {
         int x = 4, y = 9;
-        if (y / x < 3) {
-            x += y;
-        } else {
-            x *= y;
-        }
-        System.out.println("After if stmt, x = " + x);
+//        if (y / x < 3) {
+//            x += y;
+//        } else {
+//            x *= y;
+//        }
+        
         
          // Use a ternary operator to perform the same logic as above.
+         x = (y / x < 3) ? +y : x * y;
+         System.out.println("After if stmt, x = " + x);
     }
 }
