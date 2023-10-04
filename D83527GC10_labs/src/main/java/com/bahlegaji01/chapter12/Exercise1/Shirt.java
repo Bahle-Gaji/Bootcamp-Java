c3cef01cea8e0aa5d8990433ed9cc0e993f82db2
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-10-04 14:07:37 +0200

    Create Item.java

---
+++ D83527GC10_labs/src/main/java/com/bahlegaji01/chapter12/Exercise1/Shirt.java
@@ -0,0 +1,12 @@
+package com.bahlegaji01.chapter12.Exercise1;
+
+public class Shirt extends Item{
+    private String size;
+    private char colorCode;
+
+    public Shirt(double price, String size, char colorCode){
+        super("Shirt", price);
+        this.size = size;
+        this.colorCode = colorCode;
+    }
+}
---
+++ D83527GC10_labs/src/main/java/com/bahlegaji01/chapter12/Exercise1/ShoppingCart.java
@@ -0,0 +1,11 @@
+package com.bahlegaji01.chapter12.Exercise1;
+
+public class ShoppingCart {
+    public static void main(String[] args){
+
+        // instantiate a Shirt object and call display() on the object reference
+     
+            
+        }
+    
+}
