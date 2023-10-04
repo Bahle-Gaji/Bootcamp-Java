f27eb42640b61ccc2898e0ea8a87ab102960e6ab
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-10-04 14:11:26 +0200

    Complete Exercise 1
    Create myShirt Object and call display field from Item class

--- D83527GC10_labs/src/main/java/com/bahlegaji01/chapter12/Exercise1/ShoppingCart.java
+++ D83527GC10_labs/src/main/java/com/bahlegaji01/chapter12/Exercise1/ShoppingCart.java
@@ -4,7 +4,8 @@ public class ShoppingCart {
     public static void main(String[] args){
 
         // instantiate a Shirt object and call display() on the object reference
-     
+        Shirt myShirt = new Shirt(15.00, "Medium", 'B');
+        myShirt.display();
         }
     
 }
