e5d8980667cacfd01225a9e57f698d56ba66305c
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-10-04 14:27:47 +0200

    Complete Exercise 2
    Override superclass Item display() method in Shirt subclass

---
+++ D83527GC10_labs/src/main/java/com/bahlegaji01/chapter12/Exercise2/Shirt.java
@@ -0,0 +1,21 @@
+package com.bahlegaji01.chapter12.Exercise2;
+
+public class Shirt extends Item {
+    private char size;
+    private char colorCode;
+    
+    public Shirt(double price, char size, char colorCode){
+        super ("Shirt", price);
+        this.size = size;
+        this.colorCode = colorCode;
+    }
+    
+    // Override display() in the superclass to also show size and colorCode.
+    //   Avoid duplicating code.
+    public void display(){
+        super.display();
+        System.out.println("\tSize: "+size);
+        System.out.println("\tcolorCode: "+colorCode);
+    }
+    
+}
