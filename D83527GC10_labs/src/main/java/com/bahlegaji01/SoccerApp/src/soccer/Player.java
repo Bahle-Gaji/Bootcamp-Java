4402eaee0432de0c1aee2184fbe05a1ba09121a5
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-10-13 13:29:13 +0200

    Ch13P3 - Create field counting goals scored by Players

--- D83527GC10_labs/src/main/java/com/bahlegaji01/SoccerApp/src/soccer/Player.java
+++ D83527GC10_labs/src/main/java/com/bahlegaji01/SoccerApp/src/soccer/Player.java
@@ -13,7 +13,9 @@ package com.bahlegaji01.SoccerApp.src.soccer;
 public class Player {
     
     private String playerName;
+    private int goalsScored;
     
+
     public Player(String playerName) {
         this.playerName = playerName;
     }
@@ -33,5 +35,16 @@ public class Player {
     public void setPlayerName(String playerName) {
         this.playerName = playerName;
     }
+
+    public int getGoalsScored() {
+        return goalsScored;
+    }
+
+    public void setGoalsScored(int goalsScored) {
+        this.goalsScored = goalsScored;
+    }
     
+    public void incGoalsScored(){
+        this.goalsScored++;
+    }
 }
