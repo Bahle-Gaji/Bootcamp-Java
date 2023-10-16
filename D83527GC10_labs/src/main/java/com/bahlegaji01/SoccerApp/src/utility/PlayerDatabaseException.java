5ff645600d005e2f4d8168c142218f4ae832017c
Bahle-Gaji <Bahle.gaji01@gmail.com>
2023-10-16 12:19:48 +0200

    Ch14P1 - Create PlayerDatabaseException

---
+++ D83527GC10_labs/src/main/java/com/bahlegaji01/SoccerApp/src/utility/PlayerDatabaseException.java
@@ -0,0 +1,11 @@
+package com.bahlegaji01.SoccerApp.src.utility;
+
+public class PlayerDatabaseException extends Exception {
+
+    PlayerDatabaseException(){}
+
+    PlayerDatabaseException(String message){
+        super(message);
+    }
+    
+}
